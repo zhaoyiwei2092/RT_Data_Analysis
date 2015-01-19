58cb9a6f89cfc500405289d408bc2b0ad4cef382
Zhao, Yiwei <zyw@res404s-128-61-105-248.res.gatech.edu>
2015-01-18 20:55:34 -0500

    List of pairs of similar movie

--- data_download.py
+++ data_download.py
@@ -2,17 +2,29 @@ import requests
 import json
 import time
 
-respJson = list()
+movieList = list()
+similarPairs = dict()
 
 for i in range(1,7):
     address = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=9gfenez9rnk7695er96p3hxn&q=life&page_limit=50&page=' + str(i)
     resp = requests.get(address)
-    print 'Call status:', resp.status_code
-    respJson.append(json.loads(resp.text)['movies'])
+    respJson = json.loads(resp.text)
+    for j in range(0,50):
+        movieList.append(respJson['movies'][j])
     time.sleep(0.2)
 
 with open('movie_ID_name.txt', 'w') as outputFile:
     for i in range(0,300):
-        chosen = respJson[i/50][i%50]
-        outputFile.write('%s, %s\n' % (chosen['id'], chosen['title']))
+       outputFile.write('%s, %s\n' % (movieList[i]['id'], movieList[i]['title']))
+
+with open('movie_ID_sim_movie_ID.txt', 'w') as outputFile:
+    for i in range(0,300):
+        address = 'http://api.rottentomatoes.com/api/public/v1.0/movies/' + str(movieList[i]['id']) + '/similar.json?apikey=9gfenez9rnk7695er96p3hxn'
+        resp = requests.get(address)
+        respJson = json.loads(resp.text)
+        time.sleep(0.2)
+        for movie in respJson['movies']:
+            if (movie['id'],movieList[i]['id']) not in similarPairs:
+                outputFile.write('%s, %s\n' % (movieList[i]['id'], movie['id']))
+                similarPairs[(movieList[i]['id'],movie['id'])] = 1
 
